327868f7f78baea7933c933d6bf5e8cc6b77bc35
Toroi0610 <toroi0610@gmail.com>
2020-04-27 18:47:52 +0900

    for heroku

---
+++ start.js
@@ -0,0 +1,17 @@
+net = require('net');
+ 
+var sockets = [];
+ 
+var s = net.Server(function(socket) {
+ 
+    sockets.push(socket);
+ 
+    socket.on('data', function(d) {
+ 
+        for (var i=0; i < sockets.length; i++ ) {
+            sockets[i].write(d);
+        }
+    });
+});
+ 
+s.listen(process.env.PORT);
